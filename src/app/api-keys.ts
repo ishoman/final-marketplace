dd922eb5d16d6c3183cd569dd7e6e88e993e310b
Jared Lutz and Sean McDermott <jared+sean@>
2018-05-07 11:58:17 -0700

    work completed up to firebase:saving data

---
+++ src/app/api-keys.ts
@@ -0,0 +1,13 @@
+// <script src="https://www.gstatic.com/firebasejs/4.13.0/firebase.js"></script>
+// <script>
+
+  export let masterFirebaseConfig = {
+    apiKey: "AIzaSyDqNiym7WVriWz5jVfa4SuGenuBF0jQx0k",
+    authDomain: "marketplace714.firebaseapp.com",
+    databaseURL: "https://marketplace714.firebaseio.com",
+    projectId: "marketplace714",
+    storageBucket: "marketplace714.appspot.com",
+    messagingSenderId: "739025624192"
+  };
+  // firebase.initializeApp(config);
+// </script>
